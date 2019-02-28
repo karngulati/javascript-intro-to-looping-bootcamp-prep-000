8fa383370f54f2cb8f1e3fa519b06e07d00fd619
Karn Gulati <karngulati@gmail.com>
2019-02-28 01:18:20 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array) {
    for ( let i = 0; i < 25; i++) {
      if (i === 1) {
-       array.push(`I am 1 strange loop.`)
+       array.push(`I am 1 strange loop.`);
      } else {
-       array.push(`I am ${i} strange loops.`)
+       array.push(`I am ${i} strange loops.`);
      }
   }
   return array;
@@ -19,10 +19,10 @@ function whileLoop(n) {
     return "done";
 }
 
-let x = 10;
+let x = 0;
 
 function incrementVariable() {
-  x = x - 1;
+  x = x + 1;
   return x;
 }
 
