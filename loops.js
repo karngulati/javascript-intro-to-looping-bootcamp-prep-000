7c00d7624f047837de45af03cd702b4f473a44a9
Karn Gulati <karngulati@gmail.com>
2019-02-28 01:31:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -29,7 +29,7 @@ function incrementVariable() {
 function doWhileLoop(n) {
   do {
     console.log("I run once regardless.");
-} while (incrementVariable() < x);
+} while (incrementVariable() < 10);
 }
 
 
